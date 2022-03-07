77c4e7a434df95629cb02174d9acec64db0ccb80
DaniilKochnev <94305439+DaniilKochnev@users.noreply.github.com>
2022-03-08 01:13:55 +0300

    home

--- components/header.php
+++ components/header.php
@@ -11,7 +11,9 @@ if (!$_COOKIE['auth_user']) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
     <title>You task</title>
 </head>
-<body>
-<?php print_r($_COOKIE) ?>
---
+++ home.php
@@ -0,0 +1,85 @@
+<?php require_once "components/header.php"?>
+<?php
+$auth = true;
+setcookie("auth", $auth, time() - 2592000);
+?>
+<body>
+<div class="wrapper">
+    <aside class="menu">
+        <svg class="icon" style="width: 139px; height: 45px; margin-bottom: 98px;" aria-hidden="true" focusable="false">
+            <use href="images/sprite.svg#logo-blue-transparent"></use>
+        </svg>
+        <nav class="menu__navigation">
+        <ul class="menu__list">
+            <li>
+                <a href="">
+                    <svg class="icon" aria-hidden="true" focusable="false">
+                        <use href="images/sprite.svg#icon-home"></use>
+                    </svg>
+                    <span>Главная</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <svg class="icon" aria-hidden="true" focusable="false">
+                        <use href="images/sprite.svg#icon-task"></use>
+                    </svg>
+                    <span>Мои задачи</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <svg class="icon" aria-hidden="true" focusable="false">
+                        <use href="images/sprite.svg#icon-projects"></use>
+                    </svg>
+                    <span>Проекты</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <svg class="icon" aria-hidden="true" focusable="false">
+                        <use href="images/sprite.svg#icon-team"></use>
+                    </svg>
+                    <span>Команда</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <svg class="icon" aria-hidden="true" focusable="false">
+                        <use href="images/sprite.svg#icon-notifications"></use>
+                    </svg>
+                    <span>Уведомления</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <svg class="icon" aria-hidden="true" focusable="false">
+                        <use href="images/sprite.svg#icon-message"></use>
+                    </svg>
+                    <span>Чат</span> 
+                </a>
+            </li>
+        </ul>
+        </nav>
+        <div class="menu__active-user">
+            <a href="">
+            <svg class="icon" style="width:27px; height: 27px;" aria-hidden="true" focusable="false">
+                    <use href="images/sprite.svg#user-blue"></use>
+                </svg>
+                <span> Юлия Чернолихова</span>
+            </a>
+        </div>
+    </aside>
+    <div class="section__wrapper">
+        <section class="overdue-tasks"></section>
+        <div class="right-sections">
+            <section class="my-projects"></section>
+            <section class="chat"></section>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+
+
