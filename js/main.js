1f49018bbff92035c1282f9b123571f6d0ad87c6
DaniilKochnev <94305439+DaniilKochnev@users.noreply.github.com>
2022-03-11 17:23:11 +0300

    mytasks

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ function getRows(data) {
         type: "POST",
         url: '/engine/read.php',
         data: data,
-        success: function(response)
-        {
+        success: function (response) {
             var jsonData = JSON.parse(response);
 
             console.log(jsonData);
@@ -12,5 +11,18 @@ function getRows(data) {
         }
     });
 }
+
+
+//Вывод даты в header
+let date = new Date();
+let day = date.getDay();
+let dayNumber = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+document.getElementById('date').innerHTML = dayNumber + ' ' + months[month] + ' ' + year + ',' + ' ' + days[day];
+
 
 
