d621787c05114001a8b3215ebd2331944ee18228
DaniilKochnev <94305439+DaniilKochnev@users.noreply.github.com>
2022-05-18 00:47:24 +0300

    mytasks fix

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php require_once "components/header.php"?>
 
-<div class="row m-5 pt-5">
+<div class="row">
     <div class="col-12 col-sm-6 col-md-4 col-xl-3">
         <div class="card">
             <div class="card-body">
