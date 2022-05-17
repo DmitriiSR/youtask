fd6eaea4c4ec99b2b3f716b56a968ceb107485e4
Dmitriy Ryabukhin <dd.ryabukhin0509@gmail.com>
2022-05-17 23:52:14 +0300

    add card

--- main.php
+++ main.php
@@ -1,7 +1,36 @@
 <?php require_once "components/header.php"?>
-<!--<script>-->
-<!--    newObj('tasks');-->
-<!--</script>-->
+
+<div class="row m-5 pt-5">
+    <div class="col-12 col-sm-6 col-md-4 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <div class="pb-3 d-flex flex-wrap">
+                    <div class="badge bg-light text-wrap text-black-50 d-flex align-items-center" style="width: fit-content">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock me-1" viewBox="0 0 16 16">
+                            <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+                        </svg>
+                        <span class="">15.06.2022</span>
+                    </div>
+                    <div class="badge bg-info text-wrap text-white d-flex align-items-center ms-3" style="width: fit-content">
+                        <span class="">Учеба</span>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center flex-wrap">
+                    <button href="#" class="btn btn-success">Выполнено</button>
+                    <div class="ms-auto">
+                        <a href="#" class="card-link text-primary">Редактировать</a>
+                        <a href="#" class="card-link text-danger">Удалить</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <div class="container py-5">
     <button data-bind="click: function () { createNew('tasks') }" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addTask" >Добавить</button>
      <!-- ko foreach: viewModel.tasks -->
