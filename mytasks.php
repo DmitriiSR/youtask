dfa7930565e155d402e10492602fc9ab37b9f5ed
DaniilKochnev <94305439+DaniilKochnev@users.noreply.github.com>
2022-05-18 21:12:12 +0300

    fix

--- mytasks.php
+++ mytasks.php
@@ -74,7 +74,7 @@ setcookie("auth", $auth, time() - 2592000);
                                 <input data-bind="value: storage.get('tasks').taskdate, attr: {min: DateTime.now().toFormat('yyyy-LL-dd')}" type="date" class="form-control">
                             </label>
                             <a href="#" data-bind="click: function () {
-                                viewModel.inputsArr(viewModel.ebala(viewModel.inputsArr(), 'date'));}">
+                                viewModel.inputsArr(viewModel.returnArr(viewModel.inputsArr(), 'date'));}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg ms-3" viewBox="0 0 16 16">
                                 <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                             </svg>
@@ -106,7 +106,7 @@ setcookie("auth", $auth, time() - 2592000);
     viewModel.filter = ko.observable('all');
     viewModel.linkIndicator = 'myTasks';
     viewModel.inputsArr = ko.observableArray([]);
-    viewModel.ebala = function (arr, str) {
+    viewModel.returnArr = function (arr, str) {
         arr.splice(str);
         return arr;
     }
